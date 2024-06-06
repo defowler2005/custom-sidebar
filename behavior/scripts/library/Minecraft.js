101b5fe5c8966eeb071da4c6c0ba1c3faea4a553
defowler2005 <129338316+defowler2005@users.noreply.github.com>
2024-06-06 21:28:29 +0930

    Update Minecraft.js

--- behavior/scripts/library/Minecraft.js
+++ behavior/scripts/library/Minecraft.js
@@ -1,21 +1,21 @@
 import { world, system } from '@minecraft/server';
 
 system.runInterval(() => {
-  world.getAllPlayers().forEach((player) => {
-    player.onScreenDisplay.setTitle(
-      {
-        "rawtext": [
-          {
-            "text": `§aName: §e${player.nameTag}\n§aHealth: §e20\n§aArmor: §e10\n§aLevel: §e5`
-          }
-        ]
-      },
-      {
-        subtitle: '§l§aYour §eserver',
-        stayDuration: 20,
-        fadeInDuration: 0,
-        fadeOutDuration: 0
-      }
-    )
-  })
+    world.getAllPlayers().forEach((player) => {
+        player.onScreenDisplay.setTitle(
+            {
+                "rawtext": [
+                    {
+                        "text": `§aName: §e${player.nameTag}\n§aHealth: §e20\n§aArmor: §e10\n§aLevel: §e5`
+                    }
+                ]
+            },
+            {
+                subtitle: '§l§aYour §eserver',
+                stayDuration: 20,
+                fadeInDuration: 0,
+                fadeOutDuration: 0
+            }
+        )
+    })
 }, 10);
